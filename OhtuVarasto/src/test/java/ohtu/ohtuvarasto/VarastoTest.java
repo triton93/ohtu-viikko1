20565bd0738591b485c182104bbaadfe0ebc3a6d
Toni Oksanen <toni.oksanen@cs.helsinki.fi>
2014-09-07 22:27:12 +0300

    next commit

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -43,7 +43,7 @@ public class VarastoTest {
         varasto.lisaaVarastoon(8);
 
         // vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
-        assertEquals(3, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+        assertEquals(2, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
     @Test
